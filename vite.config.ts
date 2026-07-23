e73bec0e1e9eeb8fbbf109176b0310a344d5f2eb
yusufbayrakk94-code <yusufbayrakk94@gmail.com>
2026-07-23 22:43:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // <--- Özel domain root (kök) yerleşimi için eklendi
   server: {
     host: "::",
     port: 8080,
